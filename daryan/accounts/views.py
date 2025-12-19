de914ae95cfbf3ca657c1ab58cca566dc0fde246
M-Tinati <tinatigithub@gmail.com>
2025-12-19 19:54:46 +0330

    fix

--- daryan/accounts/views.py
+++ daryan/accounts/views.py
@@ -3,10 +3,16 @@ from django.views import View
 from django.contrib import messages
 from django.contrib.auth import login , logout , authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from .form import LoginForm
 class AccountsView(View):
+    class_form = LoginForm
     template_name = 'accounts/login.html'
     def get(self,request):
-        return render(request, self.template_name)
-    def post(self,request):
-        pass
+        form = self.class_form
+        return render(request, self.template_name,{'form':form})
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return redirect("dashboard")
+
+        return render(request, "accounts/login.html", {"form": form})
